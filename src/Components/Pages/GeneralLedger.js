cac6e7b3d422f031e982383c02b93d94035d0f99
James <jamesrivadinera@gmail.com>
2024-11-26 18:29:25 +0800

    ledger done

--- src/Components/Pages/GeneralLedger.js
+++ src/Components/Pages/GeneralLedger.js
@@ -1,89 +1,79 @@
-import React,{ useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Styles/GeneralLedger.css';
-import { YearSelection } from '../UIComponents/DateControls'
+import { YearSelection } from '../UIComponents/DateControls';
 import html2pdf from 'html2pdf.js';
 import { ToastSuccess, ToastError } from '../UIComponents/ToastComponent';
 
 const GeneralLedger = () => {
   const apiUrl = process.env.REACT_APP_API_URL;
   const [ledgerData, setLedgerData] = useState([]);
-  const [balance, setBalance] = useState();
-  const [capitalInput, setCapitalInput] = useState();
-  const [selectedYear, setSelectedYear] = useState(new Intl.DateTimeFormat('default', { year:'numeric' }).format(new Date()));
+  const [balance, setBalance] = useState(0);  // Starting balance
+  const [capitalInput, setCapitalInput] = useState(0);  // Default capital is 0
+  const [selectedYear, setSelectedYear] = useState(new Intl.DateTimeFormat('default', { year: 'numeric' }).format(new Date()));
+  
   let currentBalance = balance;
 
   const handleYearChange = (selectedYear) => {
-    setSelectedYear( new Intl.DateTimeFormat('default', {year:'numeric'}).format(selectedYear));
-  }
-
-  useEffect(() => {
-    getSalesAndExpenses();
-  }, [selectedYear])
+    setSelectedYear(new Intl.DateTimeFormat('default', { year: 'numeric' }).format(selectedYear));
+  };
 
   const getSalesAndExpenses = () => {
-    // Fetch data from the PHP script with the selectedDate
+    // Fetch data from the PHP script with the selectedYear and capitalInput
     fetch(`${apiUrl}/KampBJ-api/server/dataAnalysis/getSalesAndExpenses.php`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ selectedYear: selectedYear }),
+      body: JSON.stringify({ selectedYear: selectedYear, capital: capitalInput }), // Send capital in request body
     })
       .then(response => response.json())
       .then(data => {
         setLedgerData(data);
+        // Assuming the capital is already part of the response, otherwise, you could update the balance here
+        if (capitalInput > 0) {
+          setBalance(capitalInput); // Initialize balance with the capital input
+        }
       })
       .catch(error => {
         ToastError('Error fetching data:', error);
       });
-  }
+  };
 
   const handleInputCapitalChange = (e) => {
     const convertToFloat = e.target.value ? parseFloat(e.target.value) : 0;
     setCapitalInput(convertToFloat);
-    
-  }
+  };
 
   const handleCapitalSubmit = () => {
-
-    if(isNaN(capitalInput) || capitalInput <= 0 ) {
+    if (isNaN(capitalInput) || capitalInput <= 0) {
       ToastError('Please Enter A Valid Capital Amount');
-    }else {
-      setBalance(capitalInput);
+    } else {
+      setBalance(capitalInput); // Set the initial balance to the capital input value
+      ToastSuccess('Capital Added');
+      getSalesAndExpenses();
     }
-  }
+  };
 
-  // Handle updating balance based on debit and credit
-  const handleBalance = (credit, debit) => {
-    if(isNaN(currentBalance)){
-      return `₱ 00.00`;
-    }
-
-    if (!isNaN(credit) && credit > 0) {
-      currentBalance += credit;
-    }
-
-    if (!isNaN(debit) && debit > 0) {
-      currentBalance -= debit;
-    }
-    
-    // Format the balance to two decimal places
-    const formattedBalance = currentBalance.toLocaleString(undefined, {
+  const formatCurrency = (currency) => {
+    // Ensure debit is a valid number before applying formatting
+    const numericCurrency = parseFloat(currency);
+  
+    // Format the number first, then apply toFixed for decimal places
+    const formattedCurrency = numericCurrency.toLocaleString(undefined, {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
     });
-
-    return `₱ ${formattedBalance}`;
+    return `₱ ${formattedCurrency}`;
   };
 
   const handleDownloadPDF = async () => {
     const table = document.getElementById('general-ledger-table'); // Get the table element
     const tableWidth = table.offsetWidth;  // Get the table's current width
     const pageWidth = 210;  // A4 page width in mm (for landscape, it’s 297mm)
-  
+
     // Calculate the scale factor to fit the table width to the PDF page width
     const scale = pageWidth / tableWidth;
-  
+
     const options = {
       filename: 'General_Ledger.pdf',  // Optional: specify the filename for the PDF
       margin: 10,  // Set margins for the PDF (top, left, bottom, right)
@@ -96,7 +86,7 @@ const GeneralLedger = () => {
         orientation: 'landscape',  // Set the PDF orientation to landscape for wide tables
       },
     };
-  
+
     // Generate and save the PDF
     html2pdf().from(table).set(options).save();
   };
@@ -104,19 +94,20 @@ const GeneralLedger = () => {
   return (
     <div className='general-ledger'>
       <div className='general-ledger__header'>
-
         <div className='general-ledger__controls'>
           <div className='general-ledger__input-field-wrapper'>
-            <input type='number' placeholder='Enter Capital' className='general-ledger__input-field' onChange={handleInputCapitalChange} step="0.01"/>
+            <input
+              type='number'
+              placeholder='Enter Capital'
+              className='general-ledger__input-field'
+              onChange={handleInputCapitalChange}
+              step="0.01"
+            />
             <i className="general-ledger__input-icon fa-solid fa-right-to-bracket" title='Insert Capital' onClick={handleCapitalSubmit}></i>
           </div>
-          <i className="general-ledger__download-pdf-icon fa-solid fa-file-arrow-down" title='Download General Ledger' onClick={handleCapitalSubmit}></i>
+          <i className="general-ledger__download-pdf-icon fa-solid fa-file-arrow-down" title='Download General Ledger' onClick={handleDownloadPDF}></i>
         </div>
-        <YearSelection 
-          onChange={handleYearChange}
-          displayDate={selectedYear}
-        />
-
+        <YearSelection onChange={handleYearChange} displayDate={selectedYear} />
       </div>
 
       <div className='general-ledger__body'>
@@ -124,11 +115,9 @@ const GeneralLedger = () => {
           <table className='general-ledger__table' id='general-ledger-table'>
             <thead>
               <tr>
-                <th className='general-ledger__table-th'>Date</th>
-                <th className='general-ledger__table-th'>Time</th>
-                <th className='general-ledger__table-th'>Transaction ID</th>
-                <th className='general-ledger__table-th'>Reference No</th>
-                <th className='general-ledger__table-th'>Title</th>
+                <th className='general-ledger__table-th'>Month</th>
+                <th className='general-ledger__table-th'>Account</th>
+                <th className='general-ledger__table-th'>Description</th>
                 <th className='general-ledger__table-th'>Debit</th>
                 <th className='general-ledger__table-th'>Credit</th>
                 <th className='general-ledger__table-th'>Balance</th>
@@ -136,76 +125,27 @@ const GeneralLedger = () => {
             </thead>
             <tbody>
               {
-               ledgerData.map((ledgerItem, index) => (
-                <tr className='inventory__table-tr' key={index}>
-                  <td className='inventory__table-td'>{HandleDateDisplay(ledgerItem.date)}</td>
-                  <td className='inventory__table-td'>{HandleTimeDisplay(ledgerItem.date)}</td>
-                  <td className='inventory__table-td'>{ledgerItem.transactionId}</td>
-                  <td className='inventory__table-td'>{ledgerItem.referenceNumber}</td>
-                  <td className='inventory__table-td'>{HandleNullForTitle(ledgerItem.title, ledgerItem.date)}</td>
-                  <td className='inventory__table-td'>{HandleNullForDebit(ledgerItem.debit)}</td>
-                  <td className='inventory__table-td'>{HandleNullForCredit(ledgerItem.credit)}</td>
-                  <td className='inventory__table-td'>{handleBalance(ledgerItem.credit, ledgerItem.debit)}</td>
-                </tr>
-               ))
+                ledgerData.length > 0 ? ledgerData.map((ledgerItem, index) => (
+                  <tr className='inventory__table-tr' key={index}>
+                    <td className='inventory__table-td'>{ledgerItem.Month}</td>
+                    <td className='inventory__table-td'>{ledgerItem.Account}</td>
+                    <td className='inventory__table-td'>{ledgerItem.Description}</td>
+                    <td className='inventory__table-td'>{formatCurrency((ledgerItem.Debit * 1).toFixed(2))}</td>
+                    <td className='inventory__table-td'>{formatCurrency((ledgerItem.Credit * 1).toFixed(2))}</td>
+                    <td className='inventory__table-td'>{formatCurrency((ledgerItem.Balance * 1).toFixed(2))}</td>
+                  </tr>
+                )) : (
+                  <tr>
+                    <td colSpan="6">Please select year and enter a Capital Amount</td>
+                  </tr>
+                )
               }
             </tbody>
           </table>
         </div>
       </div>
     </div>
-  )
-}
-
-export default GeneralLedger
-
-export const HandleDateDisplay = (date) => {
-  const formatDate = date.split(' ')[0];
-
-  return formatDate;
-}
-
-export const HandleTimeDisplay = (date) => {
-  const formatTime = date.split(' ')[1];
-
-  return formatTime;
-}
-
-export const HandleNullForTitle = (title, date) => {
-  // Ensure date is valid and not undefined
-  const formattedDate = date.split(' ')[0]
-
-  return title ? title:`Sales Transaction on ${formattedDate}`;
-}
-
-export const HandleNullForDebit = (debit) => {
-  // Ensure debit is a valid number before applying formatting
-  const numericDebit = parseFloat(debit);
-  if (isNaN(numericDebit)) return '-';
-
-  // Format the number first, then apply toFixed for decimal places
-  const formattedDebit = numericDebit.toLocaleString(undefined, {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
-  return `₱ ${formattedDebit}`;
+  );
 };
 
-
-
-export const HandleNullForCredit = (credit) => {
-  // Ensure credit is a valid number before applying toFixed
-  const numericCredit = parseFloat(credit);
-
-  if (isNaN(numericCredit)) return '-';
-
-  // Format the number first, then apply toFixed for decimal places
-  const formattedCredit = numericCredit.toLocaleString(undefined, {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
-
-  return `₱ ${formattedCredit}`;
-};
-
-
+export default GeneralLedger;
