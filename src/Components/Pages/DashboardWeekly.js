fba94ad9c714af8d7239bb683b32029d0ef43fd3
James <jamesrivadinera@gmail.com>
2024-11-26 16:21:51 +0800

    GG

--- src/Components/Pages/DashboardWeekly.js
+++ src/Components/Pages/DashboardWeekly.js
@@ -197,7 +197,7 @@ const DashboardWeekly = () => {
               }}
             >
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" dy={10} tick={{ fontSize: 14 }} />
+              <XAxis dataKey="date" dy={10} tick={{ fontSize: 14 }} />
               <YAxis tick={{ fontSize: 14 }} />
               <Tooltip />
               <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" />
